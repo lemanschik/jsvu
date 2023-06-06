1b73331918d648221ba37f0772b05c88b9e5cac7
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-06-06 10:53:48 +0200

    Update download.mjs

--- shared/download.mjs
+++ shared/download.mjs
@@ -1,11 +1,10 @@
 import fsPromises from 'node:fs/promises';
 
 export const download = async (url) => {
-  let readBytes = 0;			
-							    
+  let readBytes = 0;						    
   // console.log(filePath); Prints: /tmp/jsvu-itXde2/jsvutmpf
   const filePath = `${await fsPromises.mkdtemp(path.join(os.tmpdir(), 'jsvu-')}/jsvutmpf`;
-  fetch(url).then((res) =>
+  return fetch(url).then((res) =>
   fsPromises.writeFile(filePath, res.body.pipeThrough(
   new TransformStream({ transform(data,controller){
   controller.enqueue(data);
